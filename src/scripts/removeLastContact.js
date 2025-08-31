4ee76f937e655f3b68c034aed8f4bd99aeeaee13
olegnezvuk91 <olegnezvuk@gmail.com>
2025-08-31 10:10:32 +0200

    1hw

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -5,9 +5,6 @@ export const removeLastContact = async () => {
   try {
     const contacts = await readContacts();
 
-    // contacts.splice(-1, 1);
-    //await writeContacts(contacts);
-
     const newArr = contacts.slice(1, contacts.length - 1);
     await writeContacts(newArr);
   } catch (error) {
