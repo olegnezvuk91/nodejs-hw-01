b818c3d2915a4cd7c54b9ffb50079e6ab4c5c04b
olegnezvuk91 <olegnezvuk@gmail.com>
2025-09-01 17:11:41 +0200

    fix read

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -4,6 +4,11 @@ import fs from 'node:fs/promises';
 export const readContacts = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
+
+    if (!data.trim()) {
+      return [];
+    }
+
     return JSON.parse(data);
   } catch (error) {
     console.error('Помилка читання файлу:', error);
